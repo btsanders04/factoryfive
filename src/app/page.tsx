aec99eb82a8a4f1f4b73e3c9d265f17bc0faccba
Brandon <btsanders04@gmail.com>
2025-05-20 11:50:03 -0400

    log registered users to posthog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import { stackServerApp } from "@/stack";
 import { redirect } from "next/navigation"
 import { MAIN_ROUTES } from "./routes";
-import { usePostHog } from 'posthog-js/react';
+import PostHogClient from './posthog';
 
 export default async function RootPage() {
   const user = await stackServerApp.getUser({ or: 'redirect' });
    // Identify the user in PostHog with their name
-   const posthog = usePostHog();
+   const posthog = PostHogClient();
    if (posthog) {
-    posthog.identify(posthog.get_distinct_id(), {
-      name: user.displayName
+    posthog.identify({
+      distinctId: user.id,
+      properties: {
+        name: user.displayName
+      }
     });
   }
   return redirect(MAIN_ROUTES.dashboard.link);
