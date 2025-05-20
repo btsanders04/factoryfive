ac35f54da6d400188b9ec07ef168fce617b5f958
Brandon <btsanders04@gmail.com>
2025-05-20 11:36:21 -0400

    identify logged in users to for posthog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import { stackServerApp } from "@/stack";
 import { redirect } from "next/navigation"
 import { MAIN_ROUTES } from "./routes";
+import { usePostHog } from 'posthog-js/react';
+
 export default async function RootPage() {
-  await stackServerApp.getUser({ or: 'redirect' });
+  const user = await stackServerApp.getUser({ or: 'redirect' });
+   // Identify the user in PostHog with their name
+   const posthog = usePostHog();
+   if (posthog) {
+    posthog.identify(posthog.get_distinct_id(), {
+      name: user.displayName
+    });
+  }
   return redirect(MAIN_ROUTES.dashboard.link);
 }
