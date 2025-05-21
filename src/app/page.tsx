5e9462504b1d99891b82d1d9313adaecf6e1285e
Brandon <btsanders04@gmail.com>
2025-05-20 21:39:18 -0400

    remove unnused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { stackServerApp } from "@/stack";
 import { redirect } from "next/navigation"
 import { MAIN_ROUTES } from "./routes";
-import PostHogClient from './posthog';
 
 export default async function RootPage() {
   await stackServerApp.getUser({ or: 'redirect' });
