dfd73f63267722f2a6f4dd224b6edacfc31d94a5
Brandon <btsanders04@gmail.com>
2025-05-23 14:30:30 -0400

    capture custom event for buy me a coffee

--- src/components/BuyMeCoffeeWidget.tsx
+++ src/components/BuyMeCoffeeWidget.tsx
@@ -1,11 +1,17 @@
 "use client";
+import { usePostHog } from "posthog-js/react";
 
 export default function BuyMeCoffeeWidget() {
+  const posthog = usePostHog();
+  const onClick = () => {
+    posthog.capture("buy_me_coffee_clicked"); 
+  }
   return (
     <a 
       href="https://www.buymeacoffee.com/brandonsanders" 
       target="_blank" 
       rel="noopener noreferrer"
+      onClick={onClick}
     >
       <img 
         src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" 
